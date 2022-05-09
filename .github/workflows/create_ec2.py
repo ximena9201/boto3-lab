999612af50da71825662447c4401cc1a6df391af
ximena9201 <ximenandrea.ro@gmail.com>
2022-05-09 16:32:20 -0400

    update script

--- .github/workflows/create_ec2.py
+++ .github/workflows/create_ec2.py
@@ -11,15 +11,11 @@ AMI_ID = 'ami-0c02fb55956c7d316'
 
 # User data
 user_data = """#!/bin/bash
-#!/bin/bash
-
 DOMAIN=sb.anacondaconnect.com
 LOCAL_IP=$(/usr/bin/curl -s  http://169.254.169.254/latest/meta-data/local-ipv4 | cut -d = -f 2)
 
 FQDN=$LOCAL_IP.$DOMAIN
 echo $FQDN > /etc/hostname
-
-
 
 cat >/etc/hosts <<EOF
 127.0.0.1 localhost localhost.localdomain localhost4 localhost4.localdomain4 ${FQDN}
